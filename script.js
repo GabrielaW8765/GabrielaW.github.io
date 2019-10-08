2863cc4cf3bcd1e5e523788380de5032d32c26cd
GabrielaW8765 <56265120+GabrielaW8765@users.noreply.github.com>
2019-10-08 11:05:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Gabriela"
+let userName = "GabrielaW8765";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
